de4d924eca79350336397e752bcac3a3a50545d9
James Fong <james@listia.com>
2022-09-09 07:29:37 -0700

    Update UI with mint count and add confirmation modal

--- components/XYWorldMint.tsx
+++ components/XYWorldMint.tsx
@@ -14,6 +14,10 @@ import * as rax from 'retry-axios';
 import axios from "axios";
 import Web3 from 'web3';
 import { CheckIcon } from '@heroicons/react/outline'
+import { Fragment } from 'react'
+import { Dialog, Transition } from '@headlessui/react'
+
+
 
 const XYWorldMint = ({ contract, handleReload }) => {
   const { account, library } = useWeb3React();
@@ -21,8 +25,11 @@ const XYWorldMint = ({ contract, handleReload }) => {
 
   const mint = useXYWorldPresaleMint(); // useXYWorldMint();
   const batchMint = useXYWorldBatchPresaleMint(); // useXYWorldBatchMint();
+  const [xyWorldAssetsCount, setxyWorldAssetsCount] = useState(0);
+  const [open, setOpen] = useState(false)
 
   const LIMITED_EDITION_PRICE = 0.05;
+  const LIMITED_EDITION_MAX = 20;
 
   const handleMint = async () => {
     var tokenIds = [];
@@ -120,6 +127,8 @@ const XYWorldMint = ({ contract, handleReload }) => {
       const xyAssets = await fetchAssets(XYContractAddress);
       const xyWorldAssets = await fetchAssets(XYWorldContractAddress);
 
+      setxyWorldAssetsCount(xyWorldAssets.length)
+
       xyAssets.map((asset) => {
         if (asset.token_id) {
           const found = xyWorldAssets.find(world => world.token_id == asset.token_id);
@@ -153,6 +162,7 @@ const XYWorldMint = ({ contract, handleReload }) => {
   ]
 
   return (
+    <>
     <div className="items-center text-center pt-3">
 
       <div className="text-left">
@@ -172,23 +182,37 @@ const XYWorldMint = ({ contract, handleReload }) => {
       </div>
 
       {isConnected && (
-        <div className="text-center">
-          <p>Choose up to 10 of your Coordinates to Mint at a time, 20 max (20 remaining):<br/></p>
-          <Multiselect
-          options={multiSelectOptions.options} // Options to display in the dropdown
-          selectedValues={multiSelectOptions.selectedValue} // Preselected value to persist in dropdown
-          displayValue="name" // Property name to display in the dropdown options
-          selectionLimit={10}
-          placeholder="Click to Select X,Y Coordinates"
-          className="text-black"
-          ref={multiSelectRef}
-          />
-          <br/><br/>
-          <button className="bg-og-green hover:bg-og-green-dark text-white text-x\l font-medium py-2 px-4 rounded" onClick={() => handleMint()}>
-            Mint your selected Limited Edition X,Y World Plots!
-          </button>
-          <ETHBalance />
-        </div>
+        <>
+        {xyWorldAssetsCount < LIMITED_EDITION_MAX && (
+          <div className="text-center">
+            <p>Choose up to 10 of your Coordinates to Mint at a time, limit 20 for allowlist mint ({LIMITED_EDITION_MAX - xyWorldAssetsCount} remaining):<br/></p>
+            <Multiselect
+            options={multiSelectOptions.options} // Options to display in the dropdown
+            selectedValues={multiSelectOptions.selectedValue} // Preselected value to persist in dropdown
+            displayValue="name" // Property name to display in the dropdown options
+            selectionLimit={LIMITED_EDITION_MAX - xyWorldAssetsCount < 10 ? 1 : 10}
+            placeholder="Click to Select X,Y Coordinates"
+            className="text-black"
+            ref={multiSelectRef}
+            />
+            <br/><br/>
+            <button className="bg-og-green hover:bg-og-green-dark text-white text-x\l font-medium py-2 px-4 rounded" onClick={() => handleMint()}>
+              Mint your selected Limited Edition X,Y World Plots!
+            </button>
+            <ETHBalance />
+            {xyWorldAssetsCount > 0 && (
+              <p>
+                Your Minted X,Y World Plots: <b>{xyWorldAssetsCount}</b>
+              </p>
+            )}
+          </div>
+        )}
+        {xyWorldAssetsCount >= LIMITED_EDITION_MAX && (
+          <div className="text-center">
+            <p><b>You have minted {LIMITED_EDITION_MAX} X,Y World Associative NFTs which is the maximum for the allowlist mint.</b></p>
+          </div>
+        )}
+        </>
       )}
       {!isConnected && (
         <p>
@@ -226,8 +250,64 @@ const XYWorldMint = ({ contract, handleReload }) => {
           </dl>
         </div>
       </div>
-
     </div>
+    <Transition.Root show={open} as={Fragment}>
+      <Dialog as="div" className="relative z-10" onClose={setOpen}>
+        <Transition.Child
+          as={Fragment}
+          enter="ease-out duration-300"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leave="ease-in duration-200"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
+        >
+          <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+        </Transition.Child>
+
+        <div className="fixed inset-0 z-10 overflow-y-auto">
+          <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+              enterTo="opacity-100 translate-y-0 sm:scale-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+              leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+            >
+              <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white px-4 pt-5 pb-4 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-sm sm:p-6">
+                <div>
+                  <div className="mx-auto flex h-12 w-12 items-center justify-center rounded-full bg-green-100">
+                    <CheckIcon className="h-6 w-6 text-green-600" aria-hidden="true" />
+                  </div>
+                  <div className="mt-3 text-center sm:mt-5">
+                    <Dialog.Title as="h3" className="text-lg font-medium leading-6 text-gray-900">
+                      Transaction sent
+                    </Dialog.Title>
+                    <div className="mt-2">
+                      <p className="text-sm text-gray-500">
+                        Be sure to verify your transaction on the blockchain. Allowlist wallets are allowed to mint up to {LIMITED_EDITION_MAX} before the public mint.
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                <div className="mt-5 sm:mt-6">
+                  <button
+                    type="button"
+                    className="inline-flex w-full justify-center rounded-md border border-transparent bg-blue-500 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-blue-600 focus:outline-none sm:text-sm"
+                    onClick={() => setOpen(false)}
+                  >
+                    Close
+                  </button>
+                </div>
+              </Dialog.Panel>
+            </Transition.Child>
+          </div>
+        </div>
+      </Dialog>
+    </Transition.Root>
+    </>
   );
 };
 
