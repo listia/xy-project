b835c84db1e177ff3e5380ac895ac0ecafdfacaa
James Fong <james@listia.com>
2022-09-09 03:08:23 -0700

    Format mint information and add samples

--- components/XYWorldMint.tsx
+++ components/XYWorldMint.tsx
@@ -13,6 +13,7 @@ import React, { useState, useEffect } from 'react';
 import * as rax from 'retry-axios';
 import axios from "axios";
 import Web3 from 'web3';
+import { CheckIcon } from '@heroicons/react/outline'
 
 const XYWorldMint = ({ contract, handleReload }) => {
   const { account, library } = useWeb3React();
@@ -108,21 +109,41 @@ const XYWorldMint = ({ contract, handleReload }) => {
     }
   }, [isConnected]);
 
+  const features = [
+    {
+      name: '2,500 Limited Edition',
+      description: 'A maximum of 2,500 Limited Edition X,Y World NFTs are available',
+    },
+    { name: 'Sept. 10th 16:00 UTC', description: 'Allowlist-only until Sept. 10th 16:00 UTC' },
+    { name: 'Price: 0.05 ETH', description: 'Limited Edition Price is 0.05 ETH. Free Base Editions will be available a few days after the Public Sale starts.' },
+    {
+      name: 'X,Y Project Owners',
+      description: 'Only <a href="https://opensea.io/collection/xy-coordinates">X,Y Project</a> owners can mint. Your wallet X,Y Coordinates will show up below.',
+    },
+  ]
+
   return (
     <div className="items-center text-center pt-3">
-      A maximum of 2,500 Limited Edition X,Y World NFTs are available.
-      <br/><br/>
-      Allowlist-only until Sept. 10th 16:00 UTC
-      <br/><br/>
-      Only <a href="https://opensea.io/collection/xy-coordinates">X,Y Project</a> owners can mint. Your wallet X,Y Coordinates will show up below.
-      <br/><br/>
-      Limited Edition Price: <b>0.05 ETH</b>
-      <br/><br/>
-      (Free Base Editions will be available a few days after the Public Sale starts.)
-      <br/><br/>
+
+      <div className="text-left">
+        <div className="mx-auto max-w-6xl py-4 px-4 sm:px-6 lg:py-8 lg:px-8">
+          <dl className="space-y-10 sm:grid sm:grid-cols-2 sm:gap-x-6 sm:gap-y-12 sm:space-y-0 lg:grid-cols-4 lg:gap-x-8">
+            {features.map((feature) => (
+              <div key={feature.name} className="relative">
+                <dt>
+                  <CheckIcon className="absolute h-6 w-6 text-green-500" aria-hidden="true" />
+                  <p className="ml-9 text-lg font-medium leading-6">{feature.name}</p>
+                </dt>
+                <dd className="mt-2 ml-9 text-base text-gray-400" dangerouslySetInnerHTML={{__html: feature.description}}></dd>
+              </div>
+            ))}
+          </dl>
+        </div>
+      </div>
+
       {isConnected && (
         <div className="text-center">
-          <p>Choose up to 10 of your Coordinates to Mint at a time:<br/></p>
+          <p>Choose up to 10 of your Coordinates to Mint at a time, 20 max (20 remaining):<br/></p>
           <Multiselect
           options={multiSelectOptions.options} // Options to display in the dropdown
           selectedValues={multiSelectOptions.selectedValue} // Preselected value to persist in dropdown
@@ -151,6 +172,31 @@ const XYWorldMint = ({ contract, handleReload }) => {
         <li>- After mint, your X,Y World is linked to your X,Y Coordinate. It goes wherever your Coordinate goes.</li>
         <li>- This means X,Y World NFTs are transferrable, but only if you sell your linked X,Y Coordinate.</li>
       </ul>
+
+      <div className="text-center">
+        <div className="mx-auto max-w-6xl py-4 px-4 sm:px-6 lg:py-8 lg:px-8">
+          <dl className="space-y-10 sm:grid sm:grid-cols-2 sm:gap-x-6 sm:gap-y-12 sm:space-y-0 lg:grid-cols-4 lg:gap-x-8">
+
+            <div className="relative">
+              <img className="mx-auto" src="images/xyw_sample_1.png" />
+            </div>
+
+            <div className="relative">
+              <img className="mx-auto" src="images/xyw_sample_2.png" />
+            </div>
+
+            <div className="relative">
+              <img className="mx-auto" src="images/xyw_sample_3.png" />
+            </div>
+
+            <div className="relative">
+              <img className="mx-auto" src="images/xyw_sample_4.png" />
+            </div>
+
+          </dl>
+        </div>
+      </div>
+
     </div>
   );
 };
